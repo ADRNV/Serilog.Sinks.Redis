3d1715b6a919e01f65e7c057849f4411632f0254
DESKTOP-IOHGBT9\AnyHDD <combinerk@gmail.com>
2025-09-12 19:26:18 +0300

    Recove solution. Add Base writer

---
+++ Serilog.Sinks.Redis/Serilog.Sink.Redis.Writer/Store/KeyGenerators.cs
@@ -0,0 +1,29 @@
+﻿using Seriog.Sink.Redis.Core.Store;
+using StackExchange.Redis;
+
+namespace Serilog.Sink.Redis.Writer.Store
+{
+    public class GuidV7Generator(IFormatProvider formatProvider) : IKeyGenerator
+    {
+        public IFormatProvider FormatProvider { get => formatProvider; }
+
+        public RedisKey Generate(params object[] options)
+        {
+            var guid = Guid.CreateVersion7();
+
+            return new RedisKey(guid.ToString());
+        }
+    }
+
+    public class TimeStampKeyGenerator(IFormatProvider formatProvider) : IKeyGenerator
+    {
+        public IFormatProvider FormatProvider { get => formatProvider; }
+
+        public RedisKey Generate(params object[] options)
+        {
+            var timeStamp = DateTime.Now;
+
+            return new RedisKey(timeStamp.ToString(formatProvider));
+        }
+    }
+}
---
+++ Serilog.Sinks.Redis/Serilog.Sink.Redis.Writer/Store/RedisDbWriter.cs
@@ -0,0 +1,43 @@
+﻿using Serilog.Debugging;
+using Serilog.Events;
+using Seriog.Sink.Redis.Core.Options;
+using Seriog.Sink.Redis.Core.Store;
+using StackExchange.Redis;
+
+namespace Serilog.Sink.Redis.Writer.Store
+{
+    public class RedisDbWriter(IConnectionMultiplexer multiplexer, IRedisKeyValueOptions options) : IRedisDbWriter
+    {
+        private readonly IKeyGenerator _keyGenerator = options.KeyGenerator;
+        public void Write(LogEvent log, IFormatProvider? formatProvider = null)
+        {
+            HashEntry[] logEntry = new HashEntry[options.WriteKeyValues.Count];
+            
+            int entryIndex = 0;
+            
+            log.RenderMessage();
+            
+            foreach (var i in options.WriteKeyValues)
+            {
+                logEntry[entryIndex] = i.Value.PrepareValue(log, formatProvider);
+                entryIndex++;
+            }
+            
+            var key = _keyGenerator.Generate();
+
+            using (multiplexer)
+            {
+                if (multiplexer.IsConnected)
+                {
+                    multiplexer.GetDatabase().HashSet(key, logEntry);
+                    SelfLog.WriteLine("[{0}] Log message wrote [{1}] with ID [{2}]", arg0: log.Timestamp, arg1: log.SpanId, arg2: key);
+                }
+                else
+                {
+                    SelfLog.WriteLine("[{0}] Failed to wrote message [{1}] with ID [{2}]", arg0: log.Timestamp, arg1: log.SpanId, arg2: key);
+                }
+            }
+            
+        }
+    }
+}
